6f175aaec4e58bbf2240411dc025ee16f3eb0472
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-07-16 21:27:46 +0300

    beautifulsoup modification

--- beautifulsoup/main.py
+++ beautifulsoup/main.py
@@ -36,12 +36,9 @@ for page in range(1, last_page + 1)[:2]:
             box = soup.find("article", class_="main-article")
             title = box.find('h1').get_text(strip=True)
             transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
-            
             # Ensure the title is valid for a filename
             valid_title = "".join([c if c.isalnum() else "_" for c in title])
-            
             with open(f'{valid_title}.txt', "w", encoding="utf-8") as file:
                 file.write(transcript)
-                
         except Exception as e:
             print(f"Failed to process link {link}: {e}")
